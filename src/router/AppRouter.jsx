6a1f21e0a9c83c4ba031855ba1acd1e4ecdd63ed
Cristian Sena <crisianjsena@gmail.com>
2024-12-17 20:08:18 -0300

    Agrego rutas

---
+++ src/router/AppRouter.jsx
@@ -0,0 +1,20 @@
+import { Navigate, Route, Routes } from 'react-router-dom';
+import { CalendarPage } from '../calendar';
+import { LoginPage } from '../auth';
+
+export const AppRouter = () => {
+
+  const authStatus = 'not-authenticated';
+
+  return (
+    <Routes>
+      {
+        ( authStatus === 'not-authenticated' )
+          ? <Route path="/auth/*" element={ <LoginPage /> } />
+          : <Route path="/*" element={ <CalendarPage /> } />
+      }
+        <Route path="/*" element={ <Navigate to="/auth/login" /> } />
+      
+    </Routes>
+  )
+}
